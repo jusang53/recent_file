fc5a1bfdc687f0270af2e248ac77c4461dc006a4
jusang53 <98146496+jusang53@users.noreply.github.com>
2022-03-13 21:18:24 +0900

    Update rightTriTest.m

--- rightTriTest.m
+++ rightTriTest.m
@@ -21,19 +21,8 @@ assert(sum(angles) == 180)
 angles = rightTri(triSkewed);
 assert(sum(angles) == 180)
 
-%% Test 2: isosceles triangles
-angles = rightTri(triIso);
-assert(angles(1) == 45)
-assert(angles(1) == angles(2))
- 
-%% Test 3: 30-60-90 triangle
+%% Test 2: 30-60-90 triangle
 angles = rightTri(tri306090);
 assert(angles(1) == 30)
 assert(angles(2) == 60)
 assert(angles(3) == 90)
-
-%% Test 4: Small angle approximation
-angles = rightTri(triSkewed);
-smallAngle = (pi/180)*angles(1); % radians
-approx = sin(smallAngle);
-assert(approx == smallAngle, 'Problem with small angle approximation')
