3b5d9dae7eb76824d1d8158b90d80dfb7f34dfd1
jusang53 <98146496+jusang53@users.noreply.github.com>
2022-06-07 21:04:54 +0900

    Update InvertedPendulumTest.m

--- InvertedPendulumTest.m
+++ InvertedPendulumTest.m
@@ -8,21 +8,21 @@ function testQuick(testCase)
 test_quick = [1 0.5 5];
 actSolution = stabilize2ref(test_quick);
 expSolution = 0.5;
-verifyEqual(testCase,actSolution,expSolution,"AbsTol",1e-03)
+verifyLessThanOrEqual(testCase,actSolution,expSolution,"AbsTol",1e-03)
 end
 
 function testProper(testCase)
 test_proper = [1 0.5 10];
 actSolution = stabilize2ref(test_proper);
 expSolution = 0.5;
-verifyEqual(testCase,actSolution,expSolution,"AbsTol",1e-03)
+verifyLessThanOrEqual(testCase,actSolution,expSolution,"AbsTol",1e-03)
 end
 
 function testSlow(testCase)
 test_slow = [1 0.5 15];
 actSolution = stabilize2ref(test_slow);
 expSolution = 0.5;
-verifyEqual(testCase,actSolution,expSolution,"AbsTol",1e-03)
+verifyLessThanOrEqual(testCase,actSolution,expSolution,"AbsTol",1e-03)
 end
 
 %% File fixtures  
