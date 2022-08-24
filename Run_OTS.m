bbb53b54a5f2ad6cf87a966db650d501b7a282e9
jusang53 <98146496+jusang53@users.noreply.github.com>
2022-08-25 00:18:46 +0900

    Update Run_OTS.m

--- Run_OTS.m
+++ Run_OTS.m
@@ -13,13 +13,6 @@ writeValue(bSimStart,1);
 % writeValue(bSimStart,0);
 
 pause(2)
-for i = 1:3
-    writeValue(bSimReset, 1);
-    pause(2)
-    writeValue(bSimReset, 0);
-    pause(2)
-end
-pause(2)
 
 writeValue(bSimStart,1);
 
